2ef806166b1fead48e8dc2e9fb689a6f28b0b28d
denkaty <denisselim2003@gmail.com>
2024-05-30 22:39:41 +0300

    add firebase.js

---
+++ config/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import Constants from "expo-constants";
+
+const firebaseConfig = {
+  apiKey: Constants.expoConfig.extra.apiKey,
+  authDomain: Constants.expoConfig.extra.authDomain,
+  projectId: Constants.expoConfig.extra.projectId,
+  storageBucket: Constants.expoConfig.extra.storageBucket,
+  messagingSenderId: Constants.expoConfig.extra.messagingSenderId,
+  appId: Constants.expoConfig.extra.appId,
+  databaseURL: Constants.expoConfig.extra.databaseURL,
+};
+
+initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const database = getDatabase();
